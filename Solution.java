46d2f31e337df402e942ec4116392c63ebc2e3b9
Crystal Rodriguez <32459131+cmgr77@users.noreply.github.com>
2017-11-22 09:14:08 -0500

    Add files via upload
    inital program

---
+++ Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int minimumNumber(int n, String password) {
+        // Return the minimum number of characters to make the password strong
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String password = in.next();
+        int answer = minimumNumber(n, password);
+        System.out.println(answer);
+        in.close();
+    }
+}
